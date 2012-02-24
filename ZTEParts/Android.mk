5d34432081a6a93d53b8236e46bff8c65376c708
Lalit Maganti <tilal6991@gmail.com>
2012-02-24 17:49:42 +0000

    Multiple updates

---
+++ ZTEParts/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := ZTEParts
+LOCAL_CERTIFICATE := platform
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),skate)
+include $(BUILD_PACKAGE)
+endif 
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),blade)
+include $(BUILD_PACKAGE)
+endif 
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+
