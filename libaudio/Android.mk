7c14e931e5a5861773704801e2e703dbfc6c33e4
Barry Lanning <fuzzra@gmail.com>
2012-03-04 11:49:38 +0000

    fix build

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -16,38 +16,11 @@ LOCAL_SHARED_LIBRARIES := \
 
 LOCAL_SRC_FILES:= AudioPolicyManager.cpp
 
+LOCAL_CFLAGS += -fno-short-enums
+
 ifeq ($(BOARD_HAVE_BLUETOOTH),true)
   LOCAL_CFLAGS += -DWITH_A2DP
 endif
-
-ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),skate)
-LOCAL_MODULE := audio_policy.skate
-include $(BUILD_SHARED_LIBRARY)
-else ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),blade)
-LOCAL_MODULE := audio.primary.blade
-include $(BUILD_SHARED_LIBRARY)
-else ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),blade2)
-LOCAL_MODULE := audio.primary.blade2
-include $(BUILD_SHARED_LIBRARY)
-endif
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw	
-LOCAL_STATIC_LIBRARIES += libmedia_helper	
-LOCAL_WHOLE_STATIC_LIBRARIES := libaudiohw_legacy
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_SHARED_LIBRARIES := \
-    libcutils \
-    libutils \
-    libmedia \
-    libhardware_legacy \
-    libdl
-
-LOCAL_SRC_FILES += AudioHardware.cpp
-
-LOCAL_CFLAGS += -fno-short-enums
 
 ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),skate)
 LOCAL_MODULE := audio.primary.skate
