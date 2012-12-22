73b003f32f4eaaa68a7e6f5a4538454bac926402
Lalit Maganti <lalitmaganti@gmail.com>
2012-12-22 22:27:15 +0000

    Fix libril logic

--- libril/Android.mk
+++ libril/Android.mk
@@ -1,6 +1,6 @@
 # Copyright 2006 The Android Open Source Project
 
-ifneq ($(TARGET_BOOTLOADER_BOARD_NAME),sharp)
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),sharp)
 
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
