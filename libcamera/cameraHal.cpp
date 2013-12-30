bde0582ac31b1aac63ec5a52af396368114d9539
Conn O'Griofa <connogriofa@gmail.com>
2013-12-30 19:53:36 +0000

    Camera HAL: workaround for QCOM legacy camera parameters on KitKat
    Change-Id: I0be492d9dda6a7111d36889397ce2a02c24c1c0f

--- libcamera/cameraHal.cpp
+++ libcamera/cameraHal.cpp
@@ -440,6 +440,27 @@ CameraHAL_FixupParams(android::CameraParameters &settings)
       settings.set(android::CameraParameters::KEY_VERTICAL_VIEW_ANGLE,
                    preferred_vertical_viewing_angle);
    }
+
+   if (settings.get(android::CameraParameters::KEY_MAX_CONTRAST)) {
+      settings.set("max-contrast",
+         settings.get(android::CameraParameters::KEY_MAX_CONTRAST));
+   } else {
+      settings.set("max-contrast", -1);
+   }
+
+   if (settings.get(android::CameraParameters::KEY_MAX_SATURATION)) {
+      settings.set("max-saturation",
+         settings.get(android::CameraParameters::KEY_MAX_SATURATION));
+   } else {
+      settings.set("max-saturation", -1);
+   }
+
+   if (settings.get(android::CameraParameters::KEY_MAX_SHARPNESS)) {
+      settings.set("max-sharpness",
+         settings.get(android::CameraParameters::KEY_MAX_SHARPNESS));
+   } else {
+      settings.set("max-sharpness", -1);
+   }
 }
 
 /* Hardware Camera interface handlers. */
