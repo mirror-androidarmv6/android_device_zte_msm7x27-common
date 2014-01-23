74beedfce3e2382772fe3873aa4a2b03065d374e
Fagyi <fagyipda@gmail.com>
2014-01-23 21:16:34 +0100

    disable FM for now
    - The last three patches from me needed merge to building correctly for the skate.
    
    Change-Id: If8c250b3da7597a87b4bdc2a496cbbebed2a09b9

--- common.mk
+++ common.mk
@@ -47,9 +47,9 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm7x27
 
 # FM Radio
-PRODUCT_PACKAGES += \
-    libfmradio.si4708 \
-    Effem
+# PRODUCT_PACKAGES += \
+#    libfmradio.si4708 \
+#    Effem
 
 # Audio
 PRODUCT_PACKAGES += \
@@ -58,8 +58,8 @@ PRODUCT_PACKAGES += \
     audio.usb.default
 
 # FM Radio
-PRODUCT_COPY_FILES += \
-        frameworks/native/data/etc/com.stericsson.hardware.fm.receiver.xml:system/etc/permissions/com.stericsson.hardware.fm.receiver.xml
+# PRODUCT_COPY_FILES += \
+#        frameworks/native/data/etc/com.stericsson.hardware.fm.receiver.xml:system/etc/permissions/com.stericsson.hardware.fm.receiver.xml
 
 # Media and vold
 PRODUCT_COPY_FILES += \
