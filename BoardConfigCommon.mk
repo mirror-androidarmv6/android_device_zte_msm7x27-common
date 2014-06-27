d35e350ef5dea38d958218abba09c2f5f4ab3863
Fagyi <fagyipda@gmail.com>
2014-06-27 22:01:53 +0200

    Multiple updates - Synced with my tree
    Change-Id: Ibaab29896187031533a597cb632f00040ddd0176

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -26,16 +26,26 @@ TARGET_SPECIFIC_HEADER_PATH := device/zte/msm7x27-common/include
 TARGET_BOOTLOADER_BOARD_NAME := sharp
 
 # Recovery
+TARGET_RECOVERY_FSTAB := device/zte/skate/fstab.skate
+TARGET_PREBUILT_RECOVERY_KERNEL := device/zte/skate/recovery/recovery_kernel
 # BOARD_CUSTOM_GRAPHICS := ../../../device/zte/msm7x27-common/recovery/graphics.c
-# BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/zte/msm7x27-common/recovery/recovery_keys.c
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/zte/msm7x27-common/recovery/recovery_keys.c
+BOARD_RECOVERY_HANDLES_MOUNT := true
+TARGET_NO_INITLOGO := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
+TARGET_NO_SEPARATE_RECOVERY := true
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/zte/msm7x27
 BOARD_KERNEL_BASE := 0x02600000
+BOARD_KERNEL_PAGESIZE := 2048
+# MINIGZIP := $(shell which lzma)
 
 # Provides
 BOARD_PROVIDES_LIBRIL := true
 TARGET_PROVIDES_LIBAUDIO := true
+BOARD_QCOM_VOIP_ENABLED := true
 TARGET_PROVIDES_LIBLIGHT := true
 
 # FM Radio
@@ -43,12 +53,32 @@ TARGET_PROVIDES_LIBLIGHT := true
 #BOARD_HAVE_QCOM_FM := true
 #COMMON_GLOBAL_CFLAGS += -DSTE_FM -DQCOM_FM_ENABLED
 
+# Bootanimation
+TARGET_BOOTANIMATION_USE_RGB565 := true
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+
+# Camera
+BOARD_USE_NASTY_PTHREAD_CREATE_HACK := true
+BOARD_CAMERA_NO_UNWANTED_MSG := true
+COMMON_GLOBAL_CFLAGS += -DICS_CAMERA_BLOB -DZTE_CAMERA_HARDWARE
+
+# Override healthd HAL
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.msm7x27
+
 # Metadata
+SKIP_SET_METADATA := true
 USE_SET_METADATA := false
 
 # USB
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun0/file
 BOARD_UMS_LUNFILE := "/sys/devices/platform/msm_hsusb/gadget/lun0/file"
+
+# SELinux
+BOARD_SEPOLICY_DIRS += device/zte/msm7x27-common/sepolicy
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    genfs_contexts
 
 ## Browser & WebKit
 ENABLE_WEBGL := true
--- common.mk
+++ common.mk
@@ -14,22 +14,8 @@
 # limitations under the License.
 #
 
-# Overlay
-DEVICE_PACKAGE_OVERLAYS := device/zte/msm7x27-common/overlay
-
-# Inherit qcom common
-$(call inherit-product, device/qcom/msm7x27/msm7x27.mk)
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
-
-# Inherit hdpi 512
-$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
-
-# AAPT
-PRODUCT_AAPT_CONFIG := normal hdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
+# Half-res bootanimation
+TARGET_BOOTANIMATION_HALF_RES := true
 
 # GPS
 PRODUCT_PACKAGES += \
@@ -41,10 +27,18 @@ PRODUCT_PACKAGES += \
 
 # Misc 
 PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory \
     lights.msm7x27 \
     sensors.msm7x27 \
-    power.msm7x27 \
-    hwcomposer.msm7x27
+    power.msm7x27 
+	
+# KSM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ksm.default=1
+    
+# WiFi
+PRODUCT_PACKAGES += \
+    libnetcmdiface
 
 # FM Radio
 # PRODUCT_PACKAGES += \
@@ -61,6 +55,19 @@ PRODUCT_PACKAGES += \
 # PRODUCT_COPY_FILES += \
 #        frameworks/native/data/etc/com.stericsson.hardware.fm.receiver.xml:system/etc/permissions/com.stericsson.hardware.fm.receiver.xml
 
+# Development & ADB authentication settings
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.debuggable=1 \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.allow.mock.location=0
+	
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.checkjni=0 \
+    dalvik.vm.debug.alloc=0 \
+    dalvik.vm.dexopt-data-only=1
+
 # Media and vold
 PRODUCT_COPY_FILES += \
     device/zte/msm7x27-common/prebuilt/vold.fstab:system/etc/vold.fstab \
@@ -70,3 +77,31 @@ PRODUCT_COPY_FILES += \
     device/zte/msm7x27-common/media/audio_policy.conf:system/etc/audio_policy.conf \
     device/zte/msm7x27-common/media/media_codecs.xml:system/etc/media_codecs.xml \
     device/zte/msm7x27-common/prebuilt/gps.conf:system/etc/gps.conf
+
+# Enable repeatable keys in CWM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.cwm.enable_key_repeat=true
+	
+## SELinux - we're not ready for enforcing mode yet
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.boot.selinux=permissive
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS := device/zte/msm7x27-common/overlay
+
+# Install/Uninstall google apps
+$(call inherit-product, vendor/google/gapps_armv6_tiny.mk)
+
+# Inherit some stuff
+$(call inherit-product, device/qcom/msm7x27/msm7x27.mk)
+$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
+$(call inherit-product, build/target/product/full.mk)
+
+# AAPT
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+PRODUCT_LOCALES := en_US en_IN fr_FR it_IT es_ES et_EE de_DE nl_NL cs_CZ \
+    pl_PL ja_JP zh_TW zh_CN zh_HK ru_RU ko_KR nb_NO es_US da_DK el_GR tr_TR \
+    pt_PT pt_BR rm_CH sv_SE bg_BG ca_ES en_GB fi_FI hr_HR hu_HU in_ID iw_IL \
+    lt_LT lv_LV ro_RO sk_SK sl_SI sr_RS uk_UA vi_VN tl_PH ar_EG fa_IR sw_TZ \
+    ms_MY af_ZA zu_ZA en_XA ar_XB fr_CA mn_MN hy_AM az_AZ ka_GE
